0e3e70dfb08461e1e7e4c3d66f138146ad12ea8a
Smah-amen <samahamen20001@gmail.com>
2024-08-22 15:23:04 +0300

    update back product module

--- backend/modules/modulesProduct.js
+++ backend/modules/modulesProduct.js
@@ -1,34 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
-name:{
-    type: 'String',
-    required: true
-},
-price: {
+  name: {
+    type: "String",
+    required: true,
+  },
+  price: {
     type: "Number",
-    required: true
-},
-// description:{
-//     type:"String",
-//     required: true
-// },
-// category:{
-//     type:"String",
-//     required: true
-// },
+    required: true,
+  },
+  description: {
+    type: "String",
+    required: true,
+  },
+  category: {
+    type: "String",
+    required: true,
+  },
 
-// isOnsale:{
-//     type:"Boolean",
-//     required: true
-// },
-// imageUrl:{
-//     type:"String",
-//     required: true
-// }
+  isOnsale: {
+    type: "Boolean",
+    required: true,
+  },
+  imageUrl: {
+    type: "String",
+    required: true,
+  },
+});
 
-
-
-})
-
-
- module.exports= mongoose.model('Products', productSchema )
+module.exports = mongoose.model("Products", productSchema);
